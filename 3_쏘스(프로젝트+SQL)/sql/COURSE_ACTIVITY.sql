669ff1224750bc67799ff42452b98d059b86aec1
신병철 <mousecjf@gmail.com>
2022-03-25 09:14:03 +0900

    Delete 3_쏘스(프로젝트+SQL) directory

--- 3_쏘스(프로젝트+SQL)/sql/COURSE_ACTIVITY.sql
+++
@@ -1,8 +0,0 @@
-create COURSE_ACTIVITY (
-	CA_NUM number constraint SEQ_PK primary key,
-	A_NUM number,
-	C_DAY number,
-	C_NUM number,
-	constraint CA_FK foreign key(C_NUM, C_DAY) references COURSE(C_NUM, C_DAY) on delete cascade
-);
-create sequence CA_SEQ increment by 1 start with 1 nocache;
--- 3_쏘스(프로젝트+SQL)/sql/COURSE_FOOD.sql
+++
@@ -1,8 +0,0 @@
-create COURSE_FOOD (
-	CF_NUM number constraint SEQ_PK primary key,
-	A_NUM number,
-	C_DAY number,
-	C_NUM number,
-	constraint CF_FK foreign key(C_NUM, C_DAY) references COURSE(C_NUM, C_DAY) on delete cascade
-);
-create sequence CF_SEQ increment by 1 start with 1 nocache;
