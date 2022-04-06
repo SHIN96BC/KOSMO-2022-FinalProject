e19a7e892cb96e6511c6996ed685fad401a24640
신병철 <mousecjf@gmail.com>
2022-04-06 14:08:45 +0900

    Delete 3_쏘스(프로젝트+SQL)/js directory

--- 3_쏘스(프로젝트+SQL)/js/makecourse/search_content_info.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function() {
-    $(document).on("click", ".content_subject",function() {
-        const contentname  = $(this).text();
-        window.open(`/jejufreinds/make_course/contentInfo.do?contentname=${contentname}`, contentname, 'width=' + screen.width + ',height=' + screen.height + ',fullscreen=yes', false);
-    });
-});
-
