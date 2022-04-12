c47f6465933c9f76a5ff265c8f41eff7356500de
신병철 <mousecjf@gmail.com>
2022-04-12 18:04:48 +0900

    Delete 3_쏘스(프로젝트+SQL)/js/selectsourse directory

--- 3_쏘스(프로젝트+SQL)/js/selectsourse/set_course_img.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.course_division').click(function() {
-        const divisionText = $(this).alt();
-        console.log("divisionText: " + divisionText);
-    });
-});
